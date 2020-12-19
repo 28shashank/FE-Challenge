25a83dfdc76c2997616413036a1d72dc3a50526c
Shashank Shekhar <28.shashank@gmail.com>
2020-12-19 12:02:26 +0530

    floating label and no result found

--- js/form-validation.js
+++ js/form-validation.js
@@ -9,8 +9,18 @@ function validateForm() {
         document.getElementById('error-email').innerHTML = "Email must be filled out, please search again";
         return false;
     }
-    else if (x == "jonsmith@example.com" || x == "janesmith@example.com" || x == "doesmith@example.com") {
-        return true;
+    else if (x != "jonsmith@example.com" || x != "janesmith@example.com" || x != "doesmith@example.com") {
+        var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (x.match(mailformat)) {
+            return true;
+        }
+        else {
+            let errLabel = document.getElementById('serachText');
+            errLabel.classList.add("error");
+            document.getElementById("floatingLabel").style.display = "none";
+            document.getElementById('error-email').innerHTML = "You have entered an invalid email address!";
+            return false;
+        }
     }
     else {
         let errLabel = document.getElementById('serachText');
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let span_Text = document.getElementById('result').innerText;
 let searchByEmail = span_Text;
 
 const params = new URLSearchParams({
-    email : `${searchByEmail}`
+    email: `${searchByEmail}`
 })
 
 //Add a parameter.
@@ -16,52 +16,54 @@ document.getElementById('apiresult').innerHTML = loader;
 //This method returns a Promise that we can use to retrieve the response of the request.
 let url = `https://cors-anywhere.herokuapp.com/https://ltv-data-api.herokuapp.com/api/v1/records.json?${params.toString()}`;
 
-fetch(url, {mode: 'cors'})
-.then ( (apidata) => {
-    //console.log(apidata);
-    return apidata.json();
-})
-.then ( (actualdata) => {
-    //console.log(actualdata);
-    var data2 = actualdata;
-    document.getElementById('apiresult').innerHTML = 
-    `<div class="card-list">
-        <span class="circle user"><img src="images/icn_person@2x.png" alt="" /></span>
-        <span class="info-container">
-            <p class="title">${data2.first_name} ${data2.last_name}, 35</p>
-            <p class="info">${data2.description}</p>
-        </span>
-    </div>
-    <div class="row card-summary">
-        <div class="col-sm">
-            <div>
-                <div class="desc-title">Address</div>
-                <div class="desc">${data2.address}</div>
+fetch(url, { mode: 'cors' })
+    .then((apidata) => {
+        return apidata.json();
+    })
+    .then((actualdata) => {
+        var data2 = actualdata;
+        if (data2.length == 0) {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById('apiresult').innerHTML = `No result found. Please, try again.`;
+        }
+        document.getElementById('apiresult').innerHTML =
+            `<div class="card-list">
+                <span class="circle user"><img src="images/icn_person@2x.png" alt="" /></span>
+                <span class="info-container">
+                    <p class="title">${data2.first_name} ${data2.last_name}, 35</p>
+                    <p class="info">${data2.description}</p>
+                </span>
             </div>
-            <div>
-                <div class="desc-title">Email</div>
-                <div class="desc">${data2.email}</div>
-            </div>
-        </div>
-        <div class="col-sm">
-            <div>
-                <div class="desc-title">Phone Numbers</div>
-                <div class="desc phone">
-                    <p>${data2.phone_numbers[0]}</p>
-                    <p>${data2.phone_numbers[1]}</p>
-                    <p>${data2.phone_numbers[2]}</p>
+            <div class="row card-summary">
+                <div class="col-sm">
+                    <div>
+                        <div class="desc-title">Address</div>
+                        <div class="desc">${data2.address}</div>
+                    </div>
+                    <div>
+                        <div class="desc-title">Email</div>
+                        <div class="desc">${data2.email}</div>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="desc-title">Relatives</div>
-                <div class="desc relatives">
-                    <p>${data2.relatives[0]}</p>
-                    <p>${data2.relatives[1]}</p>
+                <div class="col-sm">
+                    <div>
+                        <div class="desc-title">Phone Numbers</div>
+                        <div class="desc phone">
+                            <p>${data2.phone_numbers[0]}</p>
+                            <p>${data2.phone_numbers[1]}</p>
+                            <p>${data2.phone_numbers[2]}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="desc-title">Relatives</div>
+                        <div class="desc relatives">
+                            <p>${data2.relatives[0]}</p>
+                            <p>${data2.relatives[1]}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>`   
-})
-.catch ( (error) => {
-    console.log (`The Error: ${error}`);
-});
+            </div>`
+    })
+    .catch((error) => {
+        console.log(`The Error: ${error}`);
+    });
